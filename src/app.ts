62d9dba63cecf01540eb1038a8627655127d76d5
adedayojs <samfeolu@gmail.com>
2020-08-29 13:58:15 +0100

    (Fix): Fix error handler function

--- src/app.ts
+++ src/app.ts
@@ -67,23 +67,23 @@ if (fs.existsSync(clientPath)) {
 }
 
 // catch 404 and forward to error handler
-app.use(function (
-  _req: express.Request,
-  _res: express.Response,
-  next: express.NextFunction,
-) {
+app.use(function (_req: express.Request, _res: express.Response, next: express.NextFunction) {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err: any, req: express.Request, res: express.Response) {
+app.use(function (
+  err: any,
+  req: express.Request,
+  res: express.Response,
+  _next: express.NextFunction,
+) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send(err);
 });
 
 export default app;
